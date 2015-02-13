8f6ef957156d9bad60977a7e97921242fb46b0bb
Andrew Peters <andrew.r.peters.42@gmail.com>
2015-02-13 17:15:26 -0600

    Added Code to generate control data
    Still working out kinks of generating time-dependent functions

--- GeneratingSurvivalTimes.R
+++ GeneratingSurvivalTimes.R
@@ -21,3 +21,37 @@ coxphTest<-coxph(survobject~X1 + X2 + X3 + X4, data=ds)
 
 weibullTest<-survreg(survobject ~ X1 + X2+ X3 + X4, data=ds)
 
+str(weibullTest)
+weibullTest$loglik[2]
+
+
+#Functional, but apparently cannot accomodate interactions, or time-dependent covariates. So, different tactic:
+#Bender et al, 2005
+#Control Data
+#X5 and X6 are interactions between 1/3 and 2/4
+X1<-rbinom(5000, 1, 0.5)
+X2<-rbinom(5000, 1, 0.5)
+X3<-rbinom(5000, 1, 0.5)
+X4<-rbinom(5000, 1, 0.5)
+X5<-X1*X3
+X6<-X2*X4
+U<-runif(5000)
+scale<-1
+negUlog<--1*log(U)
+betaVector<-c(0.7, 0.7, 0.1, 0.1, 0.7, 0.7)
+hf<-scale*(exp(X1*betaVector[1]+X2*betaVector[2]+X3*betaVector[3]+X4*betaVector[4]+X5*betaVector[5]+X6*betaVector[6]))
+T<-negUlog/hf
+T<-T*100
+status<-1
+dsNew<-cbind(X1, X2, X3, X4, X5, X6, scale, U, negUlog, hf, T, status)
+dsNew<-as.data.frame(dsNew)
+
+
+survobject<-Surv(time=dsNew$T, event=dsNew$status)
+coxphTest<-coxph(survobject~X1 + X2 + X3 + X4 + X5 + X6, data=dsNew)
+
+weibullTest<-survreg(survobject~X1 + X2 + X3 + X4 + X5 + X6, data=dsNew, dist="weibull")
+
+#Now, the time-dependent effects
+
+
