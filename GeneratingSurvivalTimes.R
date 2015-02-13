e65cae5354b21cd45841041e0478a07481f0b2d8
Andrew Peters <andrew.r.peters.42@gmail.com>
2015-02-12 18:05:55 -0600

    Just rewrote a comment; yay for me.

--- GeneratingSurvivalTimes.R
+++ GeneratingSurvivalTimes.R
@@ -5,9 +5,7 @@ library(foreign)
 
 #http://stats.stackexchange.com/questions/109237/how-to-generate-survival-data-with-time-dependent-covariates-using-r
 
-
-#Simulating control hazard rate (f(t)=interaction with X3), at strongest level (beta=0.7).
-
+#First set of simulated, time-invariant, censorship-free survival times
 x<-list(c("bern", 0.5), c("bern", 0.5), c("bern", 0.5), c("bern", 0.5))
 b<-list(-0.7, -0.7, -0.1, -0.1)
 
