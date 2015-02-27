5b07ab52e4fd97890482016227eb56a803451488
Andrew Peters <andrew.r.peters.42@gmail.com>
2015-02-27 16:53:28 -0600

    Starting to complete for testing AIC, Heaviside

---
+++ GeneratingEVENMOREsurvivalTimes.R
@@ -0,0 +1,108 @@
+rm(list = ls(all.names = TRUE))
+library(survsim)
+library(survival)
+library(foreign)
+library(PermAlgo)
+
+
+ftw<-c(0.01, 0.7)
+propCorrect<-vector(length=length(ftw))
+
+
+for(l in 1:length(ftw)){
+  betas<-c(0.7, 0.7, 0.1, 0.1, 0, 0, 0, 0, 0, 0, ftw[l], ftw[l])
+
+  #Creating a table of AICs and BIC values
+  
+  
+  reps<-2
+  
+  fitTable<-data.frame(matrix(ncol=6, nrow=reps, ))
+  colnames(fitTable)<-c("AICH", "BICH", "AICC","BICC", "AICLog","BICLog")
+  
+  for(i in 1:reps){
+    
+    n=5000
+    m=365
+    
+    xmat<-matrix(nrow=n*m, ncol=13)
+    
+    xmat[,1]<-rep(round(rbinom(n,1, 0.5)), each=m)
+    xmat[,2]<-rep(round(rbinom(n,1, 0.5)), each=m)
+    xmat[,3]<-rep(round(rbinom(n,1, 0.5)), each=m)
+    xmat[,4]<-rep(round(rbinom(n,1, 0.5)), each=m)
+    
+    
+    #5th column is going to be column number/time interval as if we had applied counting process to a data set
+    xmat[,5]<-rep.int(1:m, times=n)
+    
+    #6th and 7th columns are going to represent the log of time * Strong1 and Strong2, respectively.
+    xmat[,6]<-log(xmat[,5])*xmat[,1]
+    xmat[,7]<-log(xmat[,5])*xmat[,2]
+    
+    #8th and 9th columns are going to represent time squared * Strong1 and Strong2, respectively
+    xmat[,8]<-xmat[,5]*xmat[,5]*xmat[,1]
+    xmat[,9]<-xmat[,5]*xmat[,5]*xmat[,2]
+    
+    #10th and 11th columns are going to represent interactions between Strong1*Weak1 and Strong2*Weak2
+    xmat[,10]<-xmat[,1]*xmat[,3]
+    xmat[,11]<-xmat[,2]*xmat[,4]
+    
+    #12 and 13th columns are going to represent interactions between heaviside function and strong1/strong2 respectively
+    for(j in 1:length(xmat[,5])){
+      xmat[j,12]<-ifelse(xmat[j,5]<=182, 0, xmat[j,1])  
+      xmat[j,13]<-ifelse(xmat[j,5]<=182, 0, xmat[j,2])
+    }
+    dsMaster<-as.data.frame(xmat)
+    
+    colnames(dsMaster)<-c("Strong1", "Strong2", "Weak1", "Weak2", "intForFt", "logtStrong1", "logtStrong2", "t2Strong1", "t2Strong2", "Strong1Weak1", "Strong2Weak2", "Strong1H", "Strong2H")
+    
+    #Strong and Weak variables, plust two heaviside function variables
+    dsH<-as.matrix(dsMaster[,c(1:4,6:13)])
+    eventTimesMaybe<-runif(n, 1, m)
+    
+    dataH<-permalgorithm(n, m, Xmat=dsH, XmatNames=c("Strong1", "Strong2", "Weak1", "Weak2", "logtStrong1", "logtStrong2", "t2Strong1", "t2Strong2", "Strong1Weak1", "Strong2Weak2", "Strong1H", "Strong2H"), eventRandom=eventTimesMaybe, betas=betas)
+    
+    attach(dataH)
+    survobjectH<-Surv(time=Start, time2=Stop, Event==1)
+    
+    testH<-coxph(survobjectH ~ Strong1 + Strong2 + Weak1 + Weak2 + Strong1H + Strong2H, data=dataH, ties="breslow")
+    
+    testControl<-coxph(survobjectH ~ Strong1 + Strong2 + Weak1 + Weak2 + Strong1Weak1 + Strong2Weak2, data=dataH)
+    
+    testLog<-coxph(survobjectH ~ Strong1 + Strong2 + Weak1 + Weak2 + logtStrong1 + logtStrong2, data=dataH)
+    
+    detach(dataH)
+    
+    AICH<-(-2*testH$loglik[2])+2*(length(testH$coefficients))
+    BICH<-(-2*testH$loglik[2])+(log(length(testH$coefficients)))
+    
+    AICC<-(-2*testControl$loglik[2])+2*(length(testControl$coefficients))
+    BICC<-(-2*testControl$loglik[2])+(log(length(testControl$coefficients)))
+    
+    AICLog<-(-2*testLog$loglik[2])+2*(length(testLog$coefficients))
+    BICLog<-(-2*testLog$loglik[2])+(log(length(testLog$coefficients)))
+    
+    
+    fitTable[i,1]<-AICH
+    fitTable[i,2]<-BICH
+    fitTable[i,3]<-AICC
+    fitTable[i,4]<-BICC
+    fitTable[i,5]<-AICLog
+    fitTable[i,6]<-BICLog
+    
+  }
+  
+  AICPropTable<-fitTable[,c(1,3,5)]
+  
+  
+  for(k in 1:reps){ 
+    AICPropTable$CorrectN<-ifelse(AICPropTable[k,1]==min(AICPropTable[k,1:3]),1,0)
+  }
+  
+  propCorrect[l]<-sum(AICPropTable$CorrectN)/reps
+  
+}
+
+propCorrect
+
--- GeneratingSurvivalTimes.R
+++ GeneratingSurvivalTimes.R
@@ -3,150 +3,95 @@ library(survsim)
 library(survival)
 library(foreign)
 library(PermAlgo)
-#http://stats.stackexchange.com/questions/109237/how-to-generate-survival-data-with-time-dependent-covariates-using-r
 
-#First set of simulated, time-invariant, censorship-free survival times
-x<-list(c("bern", 0.5), c("bern", 0.5), c("bern", 0.5), c("bern", 0.5))
-b<-list(-0.7, -0.7, -0.1, -0.1)
-
-firstData<-simple.surv.sim(n=500, foltime=365, dist.ev="weibull", anc.ev<-1, beta0.ev=3, anc.cens=1, beta0.cens=500, beta=b, x=x)
-firstData
-
-ds<-firstData
-colnames(ds)<-c("nid", "status", "start", "stop", "Z", "X1", "X2", "X3", "X4")
-
-survobject<-Surv(time=ds$stop, ds$status==1)
-
-coxphTest<-coxph(survobject~X1 + X2 + X3 + X4, data=ds)
-
-weibullTest<-survreg(survobject ~ X1 + X2+ X3 + X4, data=ds)
-
-str(weibullTest)
-weibullTest$loglik[2]
-
-
-#Functional, but apparently cannot accomodate interactions, or time-dependent covariates. So, different tactic:
-#Bender et al, 2005
-#Control Data
-#X5 and X6 are interactions between 1/3 and 2/4
-X1<-rbinom(5000, 1, 0.5)
-X2<-rbinom(5000, 1, 0.5)
-X3<-rbinom(5000, 1, 0.5)
-X4<-rbinom(5000, 1, 0.5)
-X5<-X1*X3
-X6<-X2*X4
-U<-runif(5000)
-scale<-1
-negUlog<--1*log(U)
-betaVector<-c(0.7, 0.7, 0.1, 0.1, 0.7, 0.7)
-hf<-scale*(exp(X1*betaVector[1]+X2*betaVector[2]+X3*betaVector[3]+X4*betaVector[4]+X5*betaVector[5]+X6*betaVector[6]))
-T<-negUlog/hf
-T<-T*100
-status<-1
-dsNew<-cbind(X1, X2, X3, X4, X5, X6, scale, U, negUlog, hf, T, status)
-dsNew<-as.data.frame(dsNew)
-
-
-survobject<-Surv(time=dsNew$T, event=dsNew$status)
-coxphTest<-coxph(survobject~X1 + X2 + X3 + X4 + X5 + X6, data=dsNew)
-
-weibullTest<-survreg(survobject~X1 + X2 + X3 + X4 + X5 + X6, data=dsNew, dist="weibull")
-
-
-## Example from PermoAlgo package
-# Prepare the matrice of covariate (Xmat)
-# Here we simulate daily exposures to 2 prescription drugs over a
-# year. Drug prescriptions can start any day of follow-up, and their
-# duration is a multiple of 7 days. There can be multiple prescriptions
-# for each individuals over the year and interuptions of drug use in
-# between.
-# Additionaly, there is a time-independant binary covarite (sex).
-n=500 # subjects
-m=365 # days
-# Generate the matrix of three covariate, in a 'long' format.
-Xmat=matrix(ncol=3, nrow=n*m)
-# time-independant binary covariate
-Xmat[,1] <- rep(rbinom(n, 1, 0.3), each=m)
-# Function to generate an individual time-dependent exposure history
-# e.g. generate prescriptions of different durations and doses.
-TDhist <- function(m){
-  start <- round(runif(1,1,m),0) # individual start date
-  duration <- 7 + 7*rpois(1,3) # in weeks
-  dose <- round(runif(1,0,10),1)
-  vec <- c(rep(0, start-1), rep(dose, duration))
-  while (length(vec)<=m){
-    intermission <- 21 + 7*rpois(1,3) # in weeks
-    duration <- 7 + 7*rpois(1,3) # in weeks
-    dose <- round(runif(1,0,10),1)
-    vec <- append(vec, c(rep(0, intermission), rep(dose, duration)))}
-  return(vec[1:m])}
-# create TD var
-Xmat[,2] <- do.call("c", lapply(1:n, function(i) TDhist(m)))
-Xmat[,3] <- do.call("c", lapply(1:n, function(i) TDhist(m)))
-
-head(Xmat)
-
-# genereate vectors of event and censoring times prior to calling the
-# function for the algorithm
-eventRandom <- round(rexp(n, 0.012)+1,0)
-censorRandom <- round(runif(n, 1,870),0)
-# Generate the survival data conditional on the three covariates
-
-data <- permalgorithm(n, m, Xmat, XmatNames=c("sex", "Drug1", "Drug2"),
-                      eventRandom = eventRandom, censorRandom=censorRandom, betas=c(log(2),
-                                                                                    log(1.04), log(0.99)), groupByD=FALSE )
-head(data)
-# could use survival library and check whether the data was generated
-# properly using coxph(Surv(Start, Stop, Event) ~ sex + Drug1 + Drug2,
-# data)
-
-test<-coxph(Surv(Start, Stop, Event) ~ sex + Drug1 + Drug2, data=data)
-
-
-##So...my turn.
-#Generate random survival times, random events; counting process expasion; add time-dependent element and use that for xmat in PermoAlgo.
-rm(list = ls(all.names = TRUE))
 n=5000
 m=365
 
-xmat<-matrix(nrow=n*m, ncol=11)
+xmat<-matrix(nrow=n*m, ncol=13)
 
 xmat[,1]<-rep(round(rbinom(n,1, 0.5)), each=m)
 xmat[,2]<-rep(round(rbinom(n,1, 0.5)), each=m)
 xmat[,3]<-rep(round(rbinom(n,1, 0.5)), each=m)
 xmat[,4]<-rep(round(rbinom(n,1, 0.5)), each=m)
 
+
 #5th column is going to be column number/time interval as if we had applied counting process to a data set
 xmat[,5]<-rep.int(1:m, times=n)
+
+#6th and 7th columns are going to represent the log of time * Strong1 and Strong2, respectively.
 xmat[,6]<-log(xmat[,5])*xmat[,1]
 xmat[,7]<-log(xmat[,5])*xmat[,2]
 
-
+#8th and 9th columns are going to represent time squared * Strong1 and Strong2, respectively
 xmat[,8]<-xmat[,5]*xmat[,5]*xmat[,1]
 xmat[,9]<-xmat[,5]*xmat[,5]*xmat[,2]
 
+#10th and 11th columns are going to represent interactions between Strong1*Weak1 and Strong2*Weak2
 xmat[,10]<-xmat[,1]*xmat[,3]
 xmat[,11]<-xmat[,2]*xmat[,4]
 
+#12 and 13th columns are going to represent interactions between heaviside function and strong1/strong2 respectively
+for(i in 1:length(xmat[,5])){
+  xmat[i,12]<-ifelse(xmat[i,5]<=182, 0, 1)  
+  xmat[i,13]<-ifelse(xmat[i,5]<=182, 0, 1)
+}
+
 dsMaster<-as.data.frame(xmat)
-head(dsMaster)
 
-colnames(dsMaster)<-c("X1", "X2", "X3", "X4", "intForFt", "logtX1", "logtX2", "tsquaredX1", "tsquaredX2", "X13", "X24")
-
+colnames(dsMaster)<-c("Strong1", "Strong2", "Weak1", "Weak2", "intForFt", "logtStrong1", "logtStrong2", "t2Strong1", "t2Strong2", "Strong1Weak1", "Strong2Weak2", "Strong1H", "Strong2H")
 
 #Now permute it!
-
-dsTest<-as.matrix(dsMaster[,c(1:4, 6, 7)])
-head(dsTest)
-?runif()
+dsLogt<-as.matrix(dsMaster[,c(1:4, 6, 7)])
 eventTimesMaybe<-runif(n, 1, m)
 
-dataTest<-permalgorithm(n, m, Xmat=dsTest, XmatNames=c("X1", "X2", "X3", "X4", "logtX1", "logtX2"), eventRandom=eventTimesMaybe, betas=c(0.7, 0.7, 0.1, 0.1, 0.7, 0.7))
-head(dataTest)
+dataLogt<-permalgorithm(n, m, Xmat=dsLogt, XmatNames=c("Strong1", "Strong2", "Weak1", "Weak2", "logtStrong1", "logtStrong2"), eventRandom=eventTimesMaybe, betas=c(0.7, 0.7, 0.1, 0.1, 0.7, 0.7))
 
-attach(dataTest)
+attach(dataLogt)
 survobjectlogt<-Surv(time=Start, time2=Stop, Event==1)
 
-test<-coxph(survobjectlogt ~ X1 + X2 + X3 + X4 + logtX1 + logtX2, data=dataTest, ties="breslow")
+test<-coxph(survobjectlogt ~ Strong1 + Strong2 + Weak1 + Weak2 + logtStrong1 + logtStrong2, data=dataLogt, ties="breslow")
 test
-detach(dataTest)
+detach(dataLogt)
+
+#Strong and Weak variables, plust two tsquared elements
+#dst2<-as.matrix(dsMaster[,c(1:4, 8, 9)])
+#eventTimesMaybe<-runif(n, 1, m)
+
+#datat2<-permalgorithm(n, m, Xmat=dst2, XmatNames=c("Strong1", "Strong2", "Weak1", "Weak2", "t2Strong1", "t2Strong2"), eventRandom=eventTimesMaybe, betas=c(0.7, 0.7, 0.1, 0.1, 0.7, 0.7))
+
+#attach(datat2)
+#survobjectt2<-Surv(time=Start, time2=Stop, Event==1)
+
+#testt2<-coxph(survobjectt2 ~ Strong1 + Strong2 + Weak1 + Weak2 + Strong1Weak1 + Strong2Weak2, data=datat2, ties="breslow")
+#testt2
+#detach(datat2)
+
+
+#Strong and Weak variables, plust two interaction variables
+dsControl<-as.matrix(dsMaster[,c(1:4, 10, 11)])
+eventTimesMaybe<-runif(n, 1, m)
+
+dataControl<-permalgorithm(n, m, Xmat=dsControl, XmatNames=c("Strong1", "Strong2", "Weak1", "Weak2", "Strong1Weak1", "Strong2Weak2"), eventRandom=eventTimesMaybe, betas=c(0.7, 0.7, 0.1, 0.1, 0.7, 0.7))
+
+attach(dataControl)
+survobjectControl<-Surv(time=Start, time2=Stop, Event==1)
+
+testControl<-coxph(survobjectControl ~ Strong1 + Strong2 + Weak1 + Weak2 + Strong1Weak1 + Strong2Weak2, data=dataControl, ties="breslow")
+testControl
+detach(dataControl)
+
+#Strong and Weak variables, plust two heaviside function variables
+dsH<-as.matrix(dsMaster[,c(1:4, 12, 13)])
+eventTimesMaybe<-runif(n, 1, m)
+
+dataH<-permalgorithm(n, m, Xmat=dsH, XmatNames=c("Strong1", "Strong2", "Weak1", "Weak2", "Strong1H", "Strong2H"), eventRandom=eventTimesMaybe, betas=c(0.7, 0.7, 0.1, 0.1, 0.7, 0.7))
+
+attach(dataH)
+survobjectH<-Surv(time=Start, time2=Stop, Event==1)
+
+testH<-coxph(survobjectH ~ Strong1 + Strong2 + Weak1 + Weak2 + Strong1H + Strong2H, data=dataH, ties="breslow")
+testH
+head(dataH)
+Model1<-lm(dataH$Stop~Strong1 + Strong2 + Weak1 + Weak2 + Strong1H + Strong2H, data=dataH)
+
+detach(dataH)
